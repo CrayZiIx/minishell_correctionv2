a68b5fdc081dd6aabc00ad9ca1a9ed2e47b50df9
Jordann Lecomte <jolecomt@c2r8p3.42lehavre.fr>
2024-02-20 23:34:30 +0100

    modified

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: jolecomt <jolecomt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 17:58:29 by jolecomt          #+#    #+#             */
-/*   Updated: 2024/02/20 23:25:31 by jolecomt         ###   ########.fr       */
+/*   Updated: 2024/02/20 23:29:41 by jolecomt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,6 +60,12 @@ typedef struct s_input
 	int		pipein;
 	int		pipeout;
 }				t_input;
+
+typedef struct s_double_str
+{
+	char *s;
+	char *set;
+}				t_double_str;
 /*---------*/
 /*--TYPES--*/
 /*---------*/
--- src/commandes/ft_cmdsubsplit.c
+++ src/commandes/ft_cmdsubsplit.c
@@ -6,7 +6,7 @@
 /*   By: jolecomt <jolecomt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/10 18:25:17 by jolecomt          #+#    #+#             */
-/*   Updated: 2024/02/20 21:30:12 by jolecomt         ###   ########.fr       */
+/*   Updated: 2024/02/20 23:33:46 by jolecomt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,40 +42,45 @@ static int	ft_count_words(char *s, char *set, int count)
 	return (count);
 }
 
-static char	**ft_fill_array(char **aux, char *s, char *set, int i[3],t_glob *g_global)
+static char	**ft_fill_array(char **aux,
+	t_double_str *dstr, int i[3], t_glob *g_global)
 {
 	int		q[2];
 
 	q[0] = 0;
 	q[1] = 0;
-	while (s && s[i[0]] != '\0')
+	while (dstr->s && dstr->s[i[0]] != '\0')
 	{
 		i[1] = i[0];
-		if (!ft_strchr(set, s[i[0]]))
+		if (!ft_strchr(dstr->set, dstr->s[i[0]]))
 		{
-			while ((!ft_strchr(set, s[i[0]]) || q[0] || q[1]) && s[i[0]])
+			while ((!ft_strchr(dstr->set, dstr->s[i[0]]) || q[0] || q[1]) \
+				&& dstr->s[i[0]])
 			{
-				q[0] = (q[0] + (!q[1] && s[i[0]] == '\'')) % 2;
-				q[1] = (q[1] + (!q[0] && s[i[0]] == '\"')) % 2;
+				q[0] = (q[0] + (!q[1] && dstr->s[i[0]] == '\'')) % 2;
+				q[1] = (q[1] + (!q[0] && dstr->s[i[0]] == '\"')) % 2;
 				i[0]++;
 			}
 		}
 		else
 			i[0]++;
-		aux[i[2]++] = ft_substr(s, i[1], i[0] - i[1], &g_global->gc);
+		aux[i[2]++] = ft_substr(dstr->s, i[1], i[0] - i[1], &g_global->gc);
 	}
 	return (aux);
 }
 
 char	**ft_cmdsubsplit(char const *s, char *set, t_glob *g_global)
 {
-	char	**aux;
-	int		nwords;
-	int		i[3];
+	char			**aux;
+	int				nwords;
+	int				i[3];
+	t_double_str	dstr;
 
 	i[0] = 0;
 	i[1] = 0;
 	i[2] = 0;
+	dstr.s = (char *)s;
+	dstr.set = set;
 	if (!s)
 		return (NULL);
 	nwords = ft_count_words((char *)s, set, 0);
@@ -84,7 +89,7 @@ char	**ft_cmdsubsplit(char const *s, char *set, t_glob *g_global)
 	aux = gc_malloc(&g_global->gc, (nwords + 1) * sizeof(char *));
 	if (aux == NULL)
 		return (NULL);
-	aux = ft_fill_array(aux, (char *)s, set, i,g_global);
+	aux = ft_fill_array(aux, &dstr, i, g_global);
 	aux[nwords] = NULL;
 	return (aux);
 }
